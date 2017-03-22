ac46f05afb52c9f35ed26fc71d7d44fd5d6cdee8
Cameron Zandkarimi <camcamcz@gmail.com>
2017-03-22 14:15:23 -0400

    added home route to routes file

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,6 +5,12 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
 			abstract: true,
 			templateUrl: 'templates/layout.tpl.html',
 			controller: 'LayoutController as layout'
+			
+		.state('root.home',{
+			url: '/',
+			templateUrl: 'templates/home.tpl.html',
+			controller: 'HomeController as home'
+		})	
 		});
 
 	$urlRouterProvider.otherwise('/');
