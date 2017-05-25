6304ce297d18cad717bffdc96bc80c3d9414de78
Cameron Zandkarimi <camcamcz@gmail.com>
2017-05-25 17:32:17 -0400

    added a scrollto javascript for the button, couldn't get it to work with angular though

--- src/js/controllers/HomeController.js
+++ src/js/controllers/HomeController.js
@@ -4,6 +4,11 @@ function HomeController () {
 		console.log('Home Controller Is Live');
 
 
+	// $scope.scrollTo = function (id){
+	// 	$location.hash(aboutme);
+	// 	$anchorScroll();
+	// };
 };
 
+// HomeController.$inject = ['$scope', '$location', '$anchorScroll'];
 export { HomeController };
